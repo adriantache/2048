1ebb2e7fa406be8c63a7533e817c74f375ec3ed3
Adrian Tache <contact@adriantache.com>
2018-03-01 15:49:05 +0200

    Finally fixed logic and display bug for btnDown

--- app/src/main/java/com/adriantache/a2048/MainActivity.java
+++ app/src/main/java/com/adriantache/a2048/MainActivity.java
@@ -2,11 +2,9 @@ package com.adriantache.a2048;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -71,29 +69,48 @@ public class MainActivity extends AppCompatActivity {
 
     //set all scores based on the board values
     private void updateScores() {
-        if (board[0][0] != 0) column11.setText(String.valueOf(board[0][0]));
-        if (board[0][1] != 0) column12.setText(String.valueOf(board[0][1]));
-        if (board[0][2] != 0) column13.setText(String.valueOf(board[0][2]));
-        if (board[0][3] != 0) column14.setText(String.valueOf(board[0][3]));
-        if (board[1][0] != 0) column21.setText(String.valueOf(board[1][0]));
-        if (board[1][1] != 0) column22.setText(String.valueOf(board[1][1]));
-        if (board[1][2] != 0) column23.setText(String.valueOf(board[1][2]));
-        if (board[1][3] != 0) column24.setText(String.valueOf(board[1][3]));
-        if (board[2][0] != 0) column31.setText(String.valueOf(board[2][0]));
-        if (board[2][1] != 0) column32.setText(String.valueOf(board[2][1]));
-        if (board[2][2] != 0) column33.setText(String.valueOf(board[2][2]));
-        if (board[2][3] != 0) column34.setText(String.valueOf(board[2][3]));
-        if (board[3][0] != 0) column41.setText(String.valueOf(board[3][0]));
-        if (board[3][1] != 0) column42.setText(String.valueOf(board[3][1]));
-        if (board[3][2] != 0) column43.setText(String.valueOf(board[3][2]));
-        if (board[3][3] != 0) column44.setText(String.valueOf(board[3][3]));
+        //set all values first to ensure values that change to zero are reset
+        column13.setText(String.valueOf(board[0][2]));
+        column12.setText(String.valueOf(board[0][1]));
+        column11.setText(String.valueOf(board[0][0]));
+        column14.setText(String.valueOf(board[0][3]));
+        column21.setText(String.valueOf(board[1][0]));
+        column22.setText(String.valueOf(board[1][1]));
+        column23.setText(String.valueOf(board[1][2]));
+        column24.setText(String.valueOf(board[1][3]));
+        column31.setText(String.valueOf(board[2][0]));
+        column32.setText(String.valueOf(board[2][1]));
+        column33.setText(String.valueOf(board[2][2]));
+        column34.setText(String.valueOf(board[2][3]));
+        column41.setText(String.valueOf(board[3][0]));
+        column42.setText(String.valueOf(board[3][1]));
+        column43.setText(String.valueOf(board[3][2]));
+        column44.setText(String.valueOf(board[3][3]));
+
+        //then remove any zeroes
+        if (column11.getText().equals("0")) column11.setText(null);
+        if (column12.getText().equals("0")) column12.setText(null);
+        if (column13.getText().equals("0")) column13.setText(null);
+        if (column14.getText().equals("0")) column14.setText(null);
+        if (column21.getText().equals("0")) column21.setText(null);
+        if (column22.getText().equals("0")) column22.setText(null);
+        if (column23.getText().equals("0")) column23.setText(null);
+        if (column24.getText().equals("0")) column24.setText(null);
+        if (column31.getText().equals("0")) column31.setText(null);
+        if (column32.getText().equals("0")) column32.setText(null);
+        if (column33.getText().equals("0")) column33.setText(null);
+        if (column34.getText().equals("0")) column34.setText(null);
+        if (column41.getText().equals("0")) column41.setText(null);
+        if (column42.getText().equals("0")) column42.setText(null);
+        if (column43.getText().equals("0")) column43.setText(null);
+        if (column44.getText().equals("0")) column44.setText(null);
     }
 
+    //generate the next number in order to continue the game
     private void generateNewNumbers(int amount) {
         Random random = new java.util.Random();
 
-        while (amount > 0 && !boardFull) {
-            detectFullBoard();
+        while (amount > 0 && !detectFullBoard()) {
 
             int row = random.nextInt(4);
             int column = random.nextInt(4);
@@ -110,7 +127,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void detectFullBoard() {
+    //detect if the board is full in order to prevent overwriting
+    private boolean detectFullBoard() {
         boolean fullBoard = true;
 
         for (int i = 0; i < 4; i++) {
@@ -119,79 +137,75 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        if (fullBoard) boardFull = true;
-        else boardFull = false;
+        return fullBoard;
     }
 
     //react to button down (starting with this since it's the simplest case)
     public void btnDown(View v) {
-        //create a new matrix to temporarily store values
-        int[][] boardCopy = new int[4][4];
-
-        //determine if board has moved
         boolean boardMoved = false;
 
-        Log.i(TAG, "boardCopy init: " + Arrays.deepToString(boardCopy));
+        //create a new matrix to temporarily store values
+        //int[][] boardCopy = new int[4][4];
 
+        //check for duplicates to sum up
         for (int j = 0; j < 4; j++) {
-            for (int i = 3; i > 0; i--) {
-                boolean changed = false;
+            for (int i = 3; i >= 0; i--) {
+                //boolean changed = false;
+
                 //change the numbers if they match
-                if (board[i][j] != 0) {
+                if (board[i][j] != 0 && i > 0) {
                     for (int k = i - 1; k >= 0; k--) {
                         if (board[k][j] == board[i][j]) {
                             int temp = board[i][j] * 2;
+                            board[i][j] = temp;
                             board[k][j] = 0;
-                            boardCopy[k][j] = temp;
+                            //boardCopy[k][j] = temp;
                             boardMoved = true;
-                            changed = true;
-                            Log.i(TAG, "btnDown: duplicate");
+                            //changed = true;
                             break;
-                        }
+                        } else if (board[k][j] != 0) break;
                     }
                 }
-                Log.i(TAG, "boardCopy: " + j + Arrays.deepToString(boardCopy));
+
                 //if we haven't changed anything, go ahead and copy the value to boardCopy
-                if (!changed) boardCopy[i][j] = board[i][j];
+                //if (!changed) boardCopy[i][j] = board[i][j];
             }
         }
-
-        Log.i(TAG, "boardCopy before removing blanks: " + Arrays.deepToString(boardCopy));
 
         //better system to remove blank spaces between cells
         for (int j = 0; j < 4; j++) {
             int counter = 3;
 
             for (int i = 3; i >= 0; i--) {
-                if (boardCopy[i][j] != 0) {
-                    boardCopy[counter][j] = boardCopy[i][j];
-                    boardCopy[i][j] = 0;
-                    if (counter != i) boardMoved = true;
+                if (board[i][j] != 0) {
+                    board[counter][j] = board[i][j];
+                    if (counter != i) {
+                        boardMoved = true;
+                        board[i][j] = 0;
+                    }
                     counter--;
                 }
             }
         }
 
-        Log.i(TAG, "boardCopy: " + Arrays.deepToString(boardCopy));
-
         //test if board actually "moved"
         if (!boardMoved) {
             //set flag to determine game loss possibility
             cannotMoveDown = true;
-
             //test for game loss
             gameLoss();
         } else {
+            //reset flag to determine game loss possibility
             cannotMoveDown = false;
 
             //copy all values back to main board
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                board[i][j] = boardCopy[i][j];
-                }
-            }
-
-            Log.i(TAG, "board: " + Arrays.deepToString(board));
+//            for (int i = 0; i < 4; i++) {
+//                for (int j = 0; j < 4; j++) {
+//                    board[i][j] = boardCopy[i][j];
+//                }
+//                //todo replace with arraycopy when everything works fine
+//                //System.arraycopy(boardCopy,0, board, 0, 4);
+//            }
 
             //add one more number
             generateNewNumbers(1);
